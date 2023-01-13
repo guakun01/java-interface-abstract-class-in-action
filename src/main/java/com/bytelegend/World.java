6a58bfd84dfba551c0781c9b103f90798033707b
YoungerKayn <79000036+YoungerKayn@users.noreply.github.com>
2023-01-13 07:12:09 +0000

    Update at 2023-01-13T07:12:09.141545Z

--- src/main/java/com/bytelegend/World.java
+++ src/main/java/com/bytelegend/World.java
@@ -20,13 +20,7 @@ public class World {
      */
     public static void everyFlyableObjectFly() {
         for (Object obj : objects) {
-            if (obj instanceof Butterfly) {
-                ((Butterfly) obj).fly();
-            } else if (obj instanceof Plane) {
-                ((Plane) obj).fly();
-            } else if (obj instanceof Bird) {
-                ((Bird) obj).fly();
-            }
+            ((Flyable) obj).fly();
         }
     }
 
@@ -37,13 +31,7 @@ public class World {
      */
     public static void everySoundMakerMakeSound() {
         for (Object obj : objects) {
-            if (obj instanceof Cat) {
-                ((Cat) obj).makeSound();
-            } else if (obj instanceof Car) {
-                ((Car) obj).makeSound();
-            } else if (obj instanceof Bird) {
-                ((Bird) obj).makeSound();
-            }
+            ((SoundMaker) obj).makeSound();
         }
     }
 
@@ -54,40 +42,45 @@ public class World {
      */
     public static void everyAnimalBreath() {
         for (Object obj : objects) {
-            if (obj instanceof Cat) {
-                ((Cat) obj).breath();
-            } else if (obj instanceof Bird) {
-                ((Bird) obj).breath();
-            } else if (obj instanceof Fish) {
-                ((Fish) obj).breath();
-            } else if (obj instanceof Butterfly) {
-                ((Butterfly) obj).breath();
-            }
+            ((Animal) obj).breath();
         }
     }
 }
 
 interface Flyable {
-    void fly();
+    default void fly() {
+    }
 }
 
 interface Animal {
-    void breath();
+    default void breath() {
+    }
 }
 
 interface SoundMaker {
-    void makeSound();
+    default void makeSound() {
+    }
 }
 
-class Pet {}
+abstract class Pet {
+    private final String name;
 
-class Car {
+    Pet(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Car implements SoundMaker {
     public void makeSound() {
         System.out.println("BEEP BEEP");
     }
 }
 
-class Bird {
+class Bird implements Animal, Flyable, SoundMaker {
     public void breath() {
         System.out.println("Bird is breathing");
     }
@@ -101,7 +94,7 @@ class Bird {
     }
 }
 
-class Butterfly {
+class Butterfly implements Animal, Flyable {
     public void breath() {
         System.out.println("Butterfly is breathing");
     }
@@ -111,35 +104,32 @@ class Butterfly {
     }
 }
 
-class Cat {
-    private final String name;
+class Cat extends Pet implements Animal, SoundMaker {
 
-    public Cat(String name) {
-        this.name = name;
+    Cat(String name) {
+        super(name);
     }
 
     public void breath() {
-        System.out.println("Cat " + name + " is breathing");
+        System.out.println("Cat " + getName() + " is breathing");
     }
 
     public void makeSound() {
-        System.out.println("Meow");
+        System.out.println("Meow Meow");
     }
 }
 
-class Fish {
-    private final String name;
-
+class Fish extends Pet implements Animal {
     public Fish(String name) {
-        this.name = name;
+        super(name);
     }
 
     public void breath() {
-        System.out.println("Fish " + name + " is breathing");
+        System.out.println("Fish " + getName() + " is breathing");
     }
 }
 
-class Plane {
+class Plane implements Flyable {
     public void fly() {
         System.out.println("Plane is flying");
     }
